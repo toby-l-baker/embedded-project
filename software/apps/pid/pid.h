18b72083e069d786e6f8fba002eeb034aae57701
leotoulet <leo_toulet@berkeley.edu>
2019-11-15 15:48:37 -0800

    First PID commit

---
+++ software/apps/pid/pid.h
@@ -0,0 +1,28 @@
+#ifndef PID_HEADER
+#define PID_HEADER
+
+
+//Proportional control
+float Kp = 0.0
+
+//Derivative constant
+float Kd = 0.0
+
+//Integral constant
+float Ki = 0.0
+
+//Antiwindup constant (use it?)
+float Kw = 0.0
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
