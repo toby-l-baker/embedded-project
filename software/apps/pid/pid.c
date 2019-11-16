a56c33b58e2191ccdcb280e45b97c24da802be41
leotoulet <leo_toulet@berkeley.edu>
2019-11-15 16:07:27 -0800

    Helper functions for PID

--- software/apps/pid/pid.c
+++ software/apps/pid/pid.c
@@ -1,1 +1,29 @@
 #include "pid.h"
+
+float integral = 0.0f;
+float derivative = 0.0f;
+
+float last_theta = -1.0f;
+int last_timestamp = -1.0f;
+
+void reset_integral(int time_stamp) {
+	integral = 0.0f;
+	last_timestamp = time_stamp;
+}
+
+void reset_derivative(float theta, int time_stamp) {
+	derivative = 0.0f;
+	last_theta = theta;
+}
+
+float update_integral(float new_theta, int time_stamp) {
+	float delta_t = time_stamp - last_timestamp;
+	integral += new_theta * delta_t;
+	return integral;
+}
+
+float compute_derivative(float theta, int time_stamp) {
+	float delta_t = time_stamp - last_timestamp;
+	float delta_theta = theta - last_theta;
+	return derivative;
+}
--- software/apps/pid/pid.h
+++ software/apps/pid/pid.h
@@ -3,25 +3,28 @@
 
 
 //Proportional control
-float Kp = 0.0
+float Kp = 0.0;
 
 //Derivative constant
-float Kd = 0.0
+float Kd = 0.0;
 
 //Integral constant
-float Ki = 0.0
+float Ki = 0.0;
 
 //Antiwindup constant (use it?)
-float Kw = 0.0
+float Kw = 0.0;
 
 
+//Takes the angle theta the bike makes with the vertical axis, return the motor
+//duty cycle required to correct the error using PID
+float duty_cycle_PID(float theta);
 
 
+//Resets the integral used in the PID. Takes the current clock time in clock cycles
+void reset_integral(int time_stamp);
 
-
-
-
-
+//Resets the derivative used in the PID. Takes current time & current angle
+void reset_derivative(float theta, int time_stamp);
 
 
 
