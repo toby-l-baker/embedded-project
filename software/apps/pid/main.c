e91ba1246c7d1792d144b3199b38e605a3a56850
leotoulet <leo_toulet@berkeley.edu>
2019-11-15 17:32:11 -0800

    Added prop controller

--- software/apps/pid/main.c
+++ software/apps/pid/main.c
@@ -1,12 +1,20 @@
 #include "pid.h"
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <math.h>
+
+
 
 int main() {
-	float[1] thetas = {0.0f};
-	int[1] time_stamps = {0};
 
-	for(int i = 0; i < 1; i++) {
+	float thetas[6] = {5.0, 4.9, 4.2, 3.1, 1.1, -0.4};
+	int time_stamps[6] = {0, 1, 2, 3, 4, 5};
+
+	for(int i = 0; i < 6; i++) {
+
 		printf("Theta = %f\n", thetas[i]);
 		printf("Timestamp = %d\n", time_stamps[i]);
-		printf("Duty cycle = %f\n", duty_cycle_PID(thetas[i], time_stamps[i]));
+		printf("Duty cycle = %f\n\n", duty_cycle_PID(thetas[i], time_stamps[i]));
 	}
 }
--- software/apps/pid/pid.c
+++ software/apps/pid/pid.c
@@ -1,10 +1,26 @@
 #include "pid.h"
+#include <stdio.h>
 
+//Proportional control
+float Kp = -1.0;
+
+//Derivative constant
+float Kd = 1.0;
+
+//Integral constant
+float Ki = -1.0;
+
+//Antiwindup constant (use it?)
+float Kw = 0.0;
+
+
+//Computation intermediaries
 float integral = 0.0f;
 float derivative = 0.0f;
 
 float last_theta = -1.0f;
 int last_timestamp = -1.0f;
+
 
 void reset_integral(int time_stamp) {
 	integral = 0.0f;
@@ -24,8 +40,14 @@ float update_integral(float new_theta, int time_stamp) {
 }
 
 float compute_derivative(float theta, int time_stamp) {
-	float delta_t = time_stamp - last_timestamp;
+	int delta_t = time_stamp - last_timestamp;
 	float delta_theta = theta - last_theta;
+
+	//If delta_t is 0, the we only have one sample
+	if (delta_t == 0) {return 0;}
+
+	derivative = delta_theta/delta_t;
+
 	return derivative;
 }
 
@@ -33,6 +55,7 @@ float duty_cycle_PID(float theta, float time_stamp) {
 
 	//Check that integral, derivative are initialized
 	if (last_timestamp < 0.0) {
+		printf("Integral and derivates not set\n");
 		reset_derivative(theta, time_stamp);
 		reset_integral(time_stamp);
 	}
@@ -49,6 +72,21 @@ float duty_cycle_PID(float theta, float time_stamp) {
 	//Update last values
 	last_theta = theta;
 	last_timestamp = time_stamp;
+
+	return duty_cycle;
+
+}
+
+
+float duty_cycle_proportionnal(float theta, float time_stamp) {
+
+	//Compute unbounded duty cycle
+	float duty_cycle = Kp*theta
+
+	//Clip dury cycle to -100 -- 100 interval
+	if (duty_cycle > 100.0f) {duty_cycle = 100.0f;}
+	if (duty_cycle < -100.0f) {duty_cycle = -100.0f;}
+
 
 	return duty_cycle;
 
