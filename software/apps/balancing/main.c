f74c736168763da5f085cd3009e582046b469efd
leotoulet <leo_toulet@berkeley.edu>
2019-11-18 21:18:09 -0800

    Added system model

--- software/apps/balancing/main.c
+++ software/apps/balancing/main.c
@@ -20,9 +20,13 @@
 #include "nrfx_saadc.h"
 #include "buckler.h"
 
+#define CW 1
+#define CCW -1
+
 
 
 int main() {
+
 
 	ret_code_t error_code = NRF_SUCCESS;
 
@@ -30,72 +34,65 @@ int main() {
 	error_code = NRF_LOG_INIT(NULL);
 	APP_ERROR_CHECK(error_code);
 	NRF_LOG_DEFAULT_BACKENDS_INIT();
+	
+	//angles_t angles;
+	//angles.theta_x = 1;
 
-	angles_t angles;
-	angles.theta_x = 1;
-
-	init_accelerometers();
-	/*
+	//init_accelerometers();
 	
 	APP_PWM_INSTANCE(PWM0,1);
-	struct motor * flywheel = create_motor(FLYWHEEL_PIN_ENABLE, FLYWHEEL_PIN_IN1, FLYWHEEL_PIN_IN2, PWM_CHANNEL_0, &PWM0);
-	struct motor * drive = create_motor(DRIVE_PIN_ENABLE, DRIVE_PIN_IN1, DRIVE_PIN_IN2, PWM_CHANNEL_1, &PWM0);
 
-
-
-	nrf_gpio_pin_set(flywheel->in1);
-    nrf_gpio_pin_clear(flywheel->in2);
-
+    struct motor * flywheel = create_motor(FLYWHEEL_PIN_ENABLE, FLYWHEEL_PIN_IN1, FLYWHEEL_PIN_IN2, PWM_CHANNEL_0, &PWM0);
+    struct motor * drive = create_motor(DRIVE_PIN_ENABLE, DRIVE_PIN_IN1, DRIVE_PIN_IN2, PWM_CHANNEL_1, &PWM0);
     
-
     app_pwm_config_t pwm1_cfg = APP_PWM_DEFAULT_CONFIG_2CH(5000L, flywheel->enable, drive->enable);
 
-    printf("1\n");
-
 	error_code = app_pwm_init(&PWM0, &pwm1_cfg, pwm_ready_callback);
-
 
     APP_ERROR_CHECK(error_code);
     app_pwm_enable(&PWM0);
 
-    printf("2\n");
 
 	//while (app_pwm_channel_duty_set(&PWM0, 0, flywheel->duty_cycle) == NRF_ERROR_BUSY);
-    set_motor_direction(flywheel, STOP);
+    //set_motor_direction(flywheel, STOP);
+
+    while (app_pwm_channel_duty_set(&PWM0, 1, 100) == NRF_ERROR_BUSY);
 
 	printf("Initialized!\n");
 
-	set_motor_direction(flywheel, FORWARD);
-	set_motor_pwm(flywheel, 50);
+	//set_motor_direction(drive, FORWARD);
+	//set_motor_pwm(drive, 25);
 
-	*/
 
 	while(1) {
 		//set_motor_direction(flywheel, FORWARD);
 		//set_motor_pwm(flywheel, 50);
 		
-		update_angles_struct(&angles);
+		//update_angles_struct(&angles);
 		
 
-		printf("%f\n",angles.theta_x);
+		//printf("%f\n",angles.theta_x);
 		
 		/*
 		float duty_cycle = duty_cycle_proportionnal(angles.theta_x);
 		uint8_t direction = STOP;
 		uint8_t duty_cycle_int = 0;
 
+		set_motor_pwm(drive, 25);
+		*/
+		/*
 		if (duty_cycle > 0) {
 			direction = CW;
-			duty_cycle_int = (uint8_t) duty_cycle;
+			duty_cycle_int = (uint8_t) 5;
 		}
 
 		else {
 			direction = CCW;
-			duty_cycle_int = (uint8_t) (-duty_cycle);
+			duty_cycle_int = (uint8_t) 5;//(-duty_cycle);
 		}
-
+	*/
 		
-		*/
+		
 		nrf_delay_ms(10);
 	}
 	return 0;
